8b8ac71f300c51aecf8bc494a50c7a51f84fdcaa
momenabdelkarim <momen.abdelkarim@gmail.com>
2017-12-23 23:59:24 -0700

    Deleting unnecessary files, providing method comments for the chatbot, reorganizing the remaining files, explaining project purpose, and providing more effective file names.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sentiMeant
+
+Like a spell-checker, except for your emotions. This project has two facets: a website that parses movie scripts, text, and audio input and outputs the predominant emotions displayed.
+
+The second facet, which I developed, utilizes IBM Watson's Tone Analyzer API to detect the emotion conveyed by a message and the method by which the emotion is being conveyed (analytical, confident, uncertain). The message is input through the sentiMeant Facebook page chatbot and then an emoji corresponding to the emotion and the method by which the emotion is conveyed is sent back to the user. I hope that this kind of application will see use in the field of aspergers, helping people with this condition more easily correspond socially over text and voice.
--- messenger-chatbot/python_server_logic.py
+++ messenger-chatbot/python_server_logic.py
@@ -1,67 +1,14 @@
-## Filename: server.py
+## Beginning of logic
 
-from flask import Flask, request
+#importing watson
 
-app = Flask(__name__)
+from watson_developer_cloud import ToneAnalyzerV3
 
-
-import os
-
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-   if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-   #      if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-   #          return "Verification token mismatch", 403
-   #      return request.args["hub.challenge"], 200
-
-    return "Hello world", 200
-
-if __name__ == '__main__':
-    app.run(host='52.40.198.72', port=8000)
-
-import requests
-
-import json
-import re
-##from watson_developer_cloud import ToneAnalyzerV3
-
-from flask import Flask, stream_with_context, request, Response, url_for
+# Importing operator for max value at which indexes
 
 import operator
 
-
-app = Flask(__name__)
-
-ACCESS_TOKEN = "EAAUwpnlCIIwBAJl6WEHQkMNRWFE2M9dgZC3acToPFVP3abpDWxfxowDV5oabAGfMrE94ozGe2JwGscM3wveATl1t0JKFJ4WqVgacEO2jTunvqxoSPtWKSQszPb1RY8cAHH5nTZCOIpj2TLsX1CSjbUwTHbT1auAk6wmsR8vQZDZD"
-
-
-
-
-
-def reply(user_id, msg):
-    data = {
-        "recipient": {"id": user_id},
-        "message": {"text": msg + 'ADD ORIGINAL MESSAGE HERE'}
-    }
-    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-    print(resp.content)
-
-
-@app.route('/', methods=['POST'])
-def handle_incoming_messages():
-    data = request.json
-    sender = data['entry'][0]['messaging'][0]['sender']['id']
-    message = data['entry'][0]['messaging'][0]['message']['text']
-    parsedmessage = analyze_tone(message)
-    reply(sender, parsedmessage)
-
-    return "ok"
+# Analyzes the message through Watson and returns emotions
 
 def analyze_tone(message):
     tone_analyzer = ToneAnalyzerV3(
@@ -72,10 +19,9 @@ def analyze_tone(message):
 
     tone = tone_analyzer.tone(message, tones='emotion, language, social', sentences='true',content_type='text/plain')
 
-    ##this is JSON being returned
-
     return parse_tone(tone)
 
+# Parses the JSON file returned by Watson into a list that can be manipulated
 def parse_tone(tone):
     eTones = ['disgust', 'fear', 'joy', 'sadness', 'anger']
     eVals = [0, 0, 0, 0, 0]
@@ -86,7 +32,7 @@ def parse_tone(tone):
     sVals = [0, 0, 0, 0, 0]
 
     # return array
-    rArr = []
+    #rArr = []
 
     # ok so now to parse our json copying example here
 
@@ -116,25 +62,16 @@ def parse_tone(tone):
 
     ##find_max_social(sVals,sTones)
 
-    return find_emoji(find_max_emotion(eVals, eTones), find_max_language(lVals, lTones))
+    return find_max_emotion(eVals, eTones, lVals, lTones)
 
+# Finds the most powerful emotion and language format expressed
+def find_max_emotion(eVals, eTones, lVals, lTones):
+    index1, value= max(enumerate(eVals), key=operator.itemgetter(1))
+    index2, value = max(enumerate(lVals), key=operator.itemgetter(1))
+    return find_emoji(eTones[index1], lTones[index2])
+    #mainEmotionValue=eTones[index]
 
-
-
-def find_max_emotion(eVals, eTones):
-    index, value= max(enumerate(eVals), key=operator.itemgetter(1))
-    global mainEmotionValue
-    mainEmotionValue=eTones[index]
-
-def find_max_language(lVals, lTones):
-    index, value= max(enumerate(lVals), key=operator.itemgetter(1))
-    mainLanguageValue=lTones[index]
-    return mainLanguageValue
-
-##def find_max_social(sVals, sTones):
-   ## index, value= max(enumerate(sVals), key=operator.itemgetter(1))
-  ##  mainSocialValue=sTones[index]
-
+# Finds a corresponding facial emoji expression for the strongest emotion and language expression
 
 def find_emoji(mainEmotionValue, mainLanguageValue):
     aDisgust = u'\U0001F61F'  # done
@@ -185,5 +122,56 @@ def find_emoji(mainEmotionValue, mainLanguageValue):
         return(tAnger)
 
 
+# Import Flask and request module from Flask
+from flask import Flask, request
+
+# Import request
+import requests
+
+# Importing JSON to test parse method
+
+import json
+# create a Flask app instance
+app = Flask(__name__)
+
+# Tokens from the Facebook page web hooks
+ACCESS_TOKEN = "EAACyXvTVM0UBAAZAgWuyPUrAg4BAhglAOBra4hKc0ZA8hoU4Py7gnqQnwA3gZAkvaIpOZCOtLr0EeeFyxHSc35dN30WjLTBINpagIL3qnrb6JIxDrMGaMm9kZCpZAIjlagNtHV5qJHguRgwwp10zMdp81Ec7eul5TGZBjPEfXeI8wZDZD"
+VERIFY_TOKEN = "secret"
+
+# method to reply to a message from the sender
+def reply(user_id, msg):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": msg}
+    }
+    # Post request using the Facebook Graph API v2.6
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    print(resp.content)
+
+# GET request to handle the verification of tokens
+@app.route('/', methods=['GET'])
+def handle_verification():
+    if request.args['hub.verify_token'] == VERIFY_TOKEN:
+        return request.args['hub.challenge']
+    else:
+        return "Invalid verification token"
+
+# POST request to handle in coming messages then call reply()
+@app.route('/', methods=['POST'])
+def handle_incoming_messages():
+    data = request.json
+    sender = data['entry'][0]['messaging'][0]['sender']['id']
+    message = data['entry'][0]['messaging'][0]['message']['text']
+    pmessage = analyze_tone(message)
+    reply(sender, pmessage)
+    reply(sender, "Your text: \"" + message + "\" conveys the above emotional and language expression. "
+                                              "Copy and paste the following expression to convey your "
+                                              "message more accurately to your recipient: \"" + message + " " + pmessage + ".\"")
+
+    return "ok"
+
+# Run the application.
 if __name__ == '__main__':
     app.run(debug=True)
+
+# ngrok utilized to expose local server to external requests. install ngrok from online or through pip, then run ngrok http 5000
